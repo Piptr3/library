2b2a36bb97e73c95b8aaed32c43c06376e50e7ea
Piptr3 <piptr24@gmail.com>
2024-12-28 17:10:17 +0100

    add button

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ function Book(name, author, year, pageCount, isRead) {
 function addBookToLibary(book) {
     myLibrary.push(book);
 }
+
+const book1 = new Book("To Kill a Mockingbird", "Harper Lee", 1960, 281, true);
+const book2 = new Book("1984", "George Orwell", 1949, 328, false);
+const book3 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 1925, 180, true);
+
+addBookToLibary(book1);
+addBookToLibary(book2);
+addBookToLibary(book3);
+
+myLibrary.forEach(book =>
+    console.log(`${book.name} by ${book.author}`)
+);
+
+
+console.log(myLibrary);
